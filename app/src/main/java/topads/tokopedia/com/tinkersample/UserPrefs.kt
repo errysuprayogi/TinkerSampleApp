b363003cbea75ea9dc3d3c3abd1fd36bf8977b99
errysuprayogi <erry.suprayogi+1@tokopedia.com>
2020-01-10 18:38:35 +0700

    convert to kotlin & add screen util

---
+++ app/src/main/java/topads/tokopedia/com/tinkersample/UserPrefs.kt
@@ -0,0 +1,32 @@
+package topads.tokopedia.com.tinkersample
+
+import android.content.Context
+import android.content.SharedPreferences
+
+/**
+ * Author errysuprayogi on 10,January,2020
+ */
+class UserPrefs private constructor(private val context: Context) {
+    private val settings: SharedPreferences
+    fun saveString(key: String?, value: String?) {
+        settings.edit().putString(key, value).commit()
+    }
+
+    fun getString(key: String?, defaultValue: String?): String? {
+        return settings.getString(key, defaultValue)
+    }
+
+    companion object {
+        private var instance: UserPrefs? = null
+        fun getInstance(context: Context): UserPrefs? {
+            if (instance == null) {
+                instance = UserPrefs(context)
+            }
+            return instance
+        }
+    }
+
+    init {
+        settings = context.getSharedPreferences("MyPref", 0)
+    }
+}
